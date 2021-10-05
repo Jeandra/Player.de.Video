806ba4048555b418df48403620257f2286efa67e
Jeandra <76909903+Jeandra@users.noreply.github.com>
2021-10-05 18:27:01 -0300

    Java Script

---
+++ playerVideo.js
@@ -0,0 +1,35 @@
+var video = document.getElementById("playerVideo");
+
+
+function playPause(){
+   if(video.paused) {
+       video.play();
+   } else{
+       video.pause();
+   }
+}
+
+function maxVolume(){
+    video.volume += 0.1;
+}
+
+function minVolume(){
+    video.volume -= 0.1;
+}
+ function mudo (){
+     video.volume = 0.0;
+        
+ }
+
+function reduzirTela(){
+    video.width = 300;
+}
+
+function telaNormal(){
+    video.width = 500;
+}
+
+function telaCheia(){
+    video.requestFullscreen();
+}
+
